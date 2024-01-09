8db7bdb9772ca4be6b3146794d548f350e19800e
zackattack <zackeryfrederiksen@seattleacademy.org>
2024-01-09 12:47:15 -0800

    reset6

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ const box2 = document.getElementById("box2")
 box2.addEventListener("click", reset);
 
 function reset() {
-    const R = (255)
-    const G = (0)
-    const B = (0)
+    const R = Math.floor(255);
+    const G = Math.floor(0);
+    const B = Math.floor(0);
+   
     box.style.backgroundColor = `rgb(${R},${G},${B})`;
 
 }
